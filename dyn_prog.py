f1c4f67ec593676fde328edcaed8014a2a7493aa
Nate Bourgeois <nbourgeois@srv-hpc02.hmi.local>
2022-08-08 17:22:12 -0400

    fixed dyn programming

--- dyn_prog.py
+++ dyn_prog.py
@@ -1,5 +1,6 @@
 # from concurrent.futures import thread
 # from re import L
+from collections import defaultdict
 from general_functions import *
 from metrics import *
 # import time
@@ -8,105 +9,243 @@ from metrics import *
 
 class Optimizer():
 
-    def __init__(self, aa_seq):
+    def __init__(self, aa_seq, tissues='Liver', ntissues=None):
         if not aa_seq.endswith('*'):
             aa_seq = aa_seq + '*'
             self.added=True
-        self.bai_weight_dict = get_bicodon_weights('Liver') 
+        
+        if type(tissues) is str:
+            self.tissues = [tissues]
+        else:
+            self.tissues = list(tissues)
+
+        for tissue in self.tissues:
+            self.bai_weight_dict = {}
+            self.bai_weight_dict[tissue] = get_bicodon_weights(tissue) 
+
+        if ntissues is None:
+            self.differential=False
+        else:
+            if type(ntissues) is str:
+                self.ntissues = [ntissues]
+            else:
+                self.ntissues = list(ntissues)
+
+            for ntissue in self.ntissues:
+                self.bai_weight_dict[ntissue] = get_bicodon_weights(ntissue) 
+                self.differential=True
+
         self.init_parameters(aa_seq)
         self.result = [None] * len(aa_seq)
         self.standby = {}
-        self.result[0] = 'ATG'
+        self.codon_chains = [None] * len(aa_seq)
+        self.codon_chains[0] = {'ATG':['ATG']}
+        self.codon_chains[1] = {}
+        for codon in self.gene_space[1]:
+            self.codon_chains[1][codon] = ['ATG',codon]
 
+        
     def optimize(self):
-        self.optimize_ind(len(self.result)-1)
+
+        self.chain_ind(len(self.result)-1)
+        self.result = self.codon_chains[-1]['TAA'].copy()
         if self.added:
+
             return(''.join(self.result[:-1]))
         else:
             return(''.join(self.result))
 
-    def optimize_ind(self,ind):
-
+   
+    def chain_ind(self,ind):
+        print(ind,end=' ')
         # for codon1 in self.gene_space[ind-2]:
-        if self.result[ind-2] is None:
-            self.optimize_ind(ind-1)
-        cmax=0
+        if self.codon_chains[ind] is None:
+            self.chain_ind(ind-1)
+
+        self.chain_bai(ind)
+        print(ind,end=' ')
+
+
+        # self.result[ind-2] = codon1
+        # self.result[ind-1] = codon2
+        # self.result[ind] = codon3  
+
+    # def optimize(self):
+
+    #     self.optimize_ind(len(self.result)-1)
+    #     if self.added:
+    #         return(''.join(self.result[:-1]))
+    #     else:
+    #         return(''.join(self.result))
+
+    # def optimize_ind(self,ind):
+
+    #     # for codon1 in self.gene_space[ind-2]:
+    #     if self.result[ind-2] is None:
+    #         self.optimize_ind(ind-1)
+
+    #     try: 
+    #         codon1,codon2,codon3 = self.chain_bai(ind)
+    #     except UnboundLocalError:
+    #         print(ind)
+    #         return
+    #     self.result[ind-2] = codon1
+    #     self.result[ind-1] = codon2
+    #     self.result[ind] = codon3  
+
+
+    def chain_bai(self, ind):
+
+        def calc_chain_bai(new_chain,tissue):
+            seq = ''.join(new_chain) + 'TAA'
+            sub_bai = get_bai(seq,self.bai_weight_dict[tissue])
+            return(sub_bai)
+
+        cmax=-999
+        self.codon_chains[ind] = {}
+        for codon in self.gene_space[ind]:
+            cmax=-999
+            if ind+1 < len(self.gene_space):
+                for codon2 in self.gene_space[ind+1]:
+                    for chain_key in self.codon_chains[ind-1]:
+                        new_chain = self.codon_chains[ind-1][chain_key].copy()
+                        new_chain.append(codon)
+                        new_chain.append(codon2)
+                                    
+                        sub_bai_list = [calc_chain_bai(new_chain,tissue) for tissue in self.tissues]
+                        sub_bai_gmean = geo_mean(sub_bai_list)
+
+                        if self.differential:
+                            sub_neg_bai_list = [calc_chain_bai(new_chain,tissue) for tissue in self.ntissues]
+                            sub_bai_gmean = sub_bai_gmean - geo_mean(sub_neg_bai_list)
+                        # if (0 in sub_bai_list):                 ## somehow zeroes appear below
+                        #     sub_bai_gmean = -2
+                        # else:
+                        if sub_bai_gmean > cmax:
+                            cmax = sub_bai_gmean
+                            self.codon_chains[ind][codon] = new_chain[:-1]
+            else:
+                for chain_key in self.codon_chains[ind-1]:
+                    new_chain = self.codon_chains[ind-1][chain_key].copy()
+                    new_chain.append(codon)
+                                
+                    sub_bai_list = [calc_chain_bai(new_chain,tissue) for tissue in self.tissues]
+                    # if (0 in sub_bai_list):                 ## somehow zeroes appear below
+                    #     sub_bai_gmean = -2
+                    # else:
+                    sub_bai_gmean = geo_mean(sub_bai_list)
+                    if sub_bai_gmean > cmax:
+                        cmax = sub_bai_gmean
+                        self.codon_chains[ind][codon] = new_chain
+                                            # if self.differential:
+                    #     tri_neg_bai_list = [calc_tri_bai(bicodon1,bicodon2,ntissue) for ntissue in self.ntissues]
+                    #     if (0 in tri_bai_list):                 ## somehow zeroes appear below
+                    #         tri_bai_gmean = -2
+                    #     else:
+                    #         tri_bai_gmean = tri_bai_gmean - geo_mean(tri_neg_bai_list)
+        
+        # return(maxcodon1,maxcodon2,maxcodon3)
+
+
+    def get_maximal_bai(self, ind):
+
+        def calc_tri_bai(bicodon1,bicodon2,tissue):
+            tri_bai = np.exp(len( self.result) * np.log( self.bai_weight_dict[tissue][bicodon1] * self.bai_weight_dict[tissue][bicodon2] ) )
+            return(tri_bai)
+
+        def calc_tri_neg_bai(bicodon1,bicodon2,tissue):
+            tri_neg_bai = np.exp(len( self.result) * np.log( self.bai_weight_dict[tissue][bicodon1] * self.bai_weight_dict[tissue][bicodon2] ) )
+            return(tri_neg_bai)
+
+        cmax=-999
         codon1 = self.result[ind-2]
         for codon2 in self.gene_space[ind-1]:
             for codon3 in self.gene_space[ind]:
                 bicodon1 = codon1 + codon2
                 bicodon2 = codon2 + codon3
-                bai_tri = np.exp(len( self.result) * np.log( self.bai_weight_dict[bicodon1] * self.bai_weight_dict[bicodon2] ) )
-                if bai_tri > cmax:
-                    cmax=bai_tri
-                    self.result[ind-2] = codon1
-                    self.result[ind-1] = codon2
-                    self.result[ind] = codon3
-           
 
+                tri_bai_list = [calc_tri_bai(bicodon1,bicodon2,tissue) for tissue in self.tissues]
+                if (0 in tri_bai_list):                 ## somehow zeroes appear below
+                    tri_bai_gmean = -2
+                else:
+                    tri_bai_gmean = geo_mean(tri_bai_list)
+
+                if self.differential:
+                    tri_neg_bai_list = [calc_tri_bai(bicodon1,bicodon2,ntissue) for ntissue in self.ntissues]
+                    if (0 in tri_bai_list):                 ## somehow zeroes appear below
+                        tri_bai_gmean = -2
+                    else:
+                        tri_bai_gmean = tri_bai_gmean - geo_mean(tri_neg_bai_list)
+
+                if tri_bai_gmean > cmax:
+                    cmax=tri_bai_gmean   
+                    maxcodon1 = codon1
+                    maxcodon2 = codon2
+                    maxcodon3 = codon3
+        return(maxcodon1,maxcodon2,maxcodon3)
 
     def init_parameters(self, aa_seq,
             codon_usage_table_loc=os.path.join(pygad_loc,'references/codon_usage.getex.txt')):
-            """Retrieve default parameters for pygad.  
+        """Retrieve default parameters for pygad.  
 
-            Pygad requires an input gene space that is integers, so the aa_seq needs to be converted to 
-            an integer list with each integer corresponding to a unique codon. The function also requires
-            allof the defualt parameters for pygad.
+        Pygad requires an input gene space that is integers, so the aa_seq needs to be converted to 
+        an integer list with each integer corresponding to a unique codon. The function also requires
+        allof the defualt parameters for pygad.
 
-            Args:
-                aa_seq (str): Input amino acid sequence to codon optimize
-                codon_usage_table_loc (str): location of codon usage table
+        Args:
+            aa_seq (str): Input amino acid sequence to codon optimize
+            codon_usage_table_loc (str): location of codon usage table
+            
+        Assigns:
+            codon_to_int (dict): dict to map codons to ints and back
+            gene_space_int (list[list]): list of lists where each index corresponds to a position
+                in the input aa_seq, and the list in that index are all synonymous codons for the aa
+                in the pos for the original seq
+            variable_dict (dict): dictionary of following defualt values for pygad
+                num_genes (int): number of genes in the gene space
+                num_generations (int): Number of generations.
+                num_parents_mating (int): Number of solutions to be selected as 
+                    parents in the mating pool.
+                sol_per_pop (int): population size 
+                parent_selection_type (str): # Type of parent selection, 
+                    sss is steady state selection 
+                keep_parents (int): Number of parents to keep in the next population.
+                        -1 means keep all parents and 0 means keep nothing.
+                crossover_type (str): Type of the crossover operator. 
+                mutation_type (str): Type of the mutation operator.
+                mutation_percent_genes (int): Percentage of genes to mutate. 
+                'stop_criteria': If the fitness fnction reaches 1, it will stop
+                'gene_type' (type): expected object type for each gene.  Genes (In our 
+                    case each codon) is uniquely mapped to an integer.
+                'on_generation' (func): callback function to be called each gen
+        """         
+
+        codon_usage_table = pd.read_csv(codon_usage_table_loc,sep='\t')
+        forward_table = pd.Series(codon_usage_table.AA.values,index=codon_usage_table.Codon).to_dict()
+
+        back_table = {}
+        for key in forward_table:
+            val = forward_table[key]
+            if val not in back_table.keys():
+                back_table[val] = [key]
+            else:
+                back_table[val].append(key)
+
+        # codon_to_int = {}
+        # i=0
+        # for codon in forward_table.keys():
+        #     codon_to_int[codon] = i
+        #     codon_to_int[i] = codon
+        #     i += 1
+
+        gene_space = []
+        for aa in aa_seq:
+            all_cds = back_table[aa]
+            gene_space.append(all_cds)
+
+        # gene_space_int = [[codon_to_int[x] for x in y] for y in gene_space]
                 
-            Assigns:
-                codon_to_int (dict): dict to map codons to ints and back
-                gene_space_int (list[list]): list of lists where each index corresponds to a position
-                    in the input aa_seq, and the list in that index are all synonymous codons for the aa
-                    in the pos for the original seq
-                variable_dict (dict): dictionary of following defualt values for pygad
-                    num_genes (int): number of genes in the gene space
-                    num_generations (int): Number of generations.
-                    num_parents_mating (int): Number of solutions to be selected as 
-                        parents in the mating pool.
-                    sol_per_pop (int): population size 
-                    parent_selection_type (str): # Type of parent selection, 
-                        sss is steady state selection 
-                    keep_parents (int): Number of parents to keep in the next population.
-                            -1 means keep all parents and 0 means keep nothing.
-                    crossover_type (str): Type of the crossover operator. 
-                    mutation_type (str): Type of the mutation operator.
-                    mutation_percent_genes (int): Percentage of genes to mutate. 
-                    'stop_criteria': If the fitness fnction reaches 1, it will stop
-                    'gene_type' (type): expected object type for each gene.  Genes (In our 
-                        case each codon) is uniquely mapped to an integer.
-                    'on_generation' (func): callback function to be called each gen
-            """         
-
-            codon_usage_table = pd.read_csv(codon_usage_table_loc,sep='\t')
-            forward_table = pd.Series(codon_usage_table.AA.values,index=codon_usage_table.Codon).to_dict()
-
-            back_table = {}
-            for key in forward_table:
-                val = forward_table[key]
-                if val not in back_table.keys():
-                    back_table[val] = [key]
-                else:
-                    back_table[val].append(key)
-
-            codon_to_int = {}
-            i=0
-            for codon in forward_table.keys():
-                codon_to_int[codon] = i
-                codon_to_int[i] = codon
-                i += 1
-
-            gene_space = []
-            for aa in aa_seq:
-                all_cds = back_table[aa]
-                gene_space.append(all_cds)
-
-            gene_space_int = [[codon_to_int[x] for x in y] for y in gene_space]
-                    
-            self.gene_space = gene_space
-            self.codon_to_int = codon_to_int
-            self.gene_space_int = gene_space_int
+        self.gene_space = gene_space
+        # self.codon_to_int = codon_to_int
+        # self.gene_space_int = gene_space_int
 
